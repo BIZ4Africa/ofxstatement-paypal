ecf6bf77fa3b60c30cd4c6afb3026287fa5ed516
Business Solutions For Africa <vincent@biz-4-africa.com>
2021-08-28 00:24:47 +0200

    Initial version

--- src/ofxstatement/plugins/paypal.py
+++ src/ofxstatement/plugins/paypal.py
@@ -1,11 +1,11 @@
 import csv
 import re
-import random
+from datetime import datetime
 
 from decimal import Decimal as D
 
 from ofxstatement import statement
-from ofxstatement.statement import generate_transaction_id
+from ofxstatement.statement import generate_transaction_id, StatementLine
 from ofxstatement.statement import generate_unique_transaction_id
 
 from ofxstatement.parser import CsvStatementParser
@@ -23,7 +23,7 @@ class PayPalPlugin(Plugin):
 
 class PayPalParser(CsvStatementParser):
 
-    date_format = "%d/%b/%y"
+    date_format = None
     mappings = {
         # 'check_no': 3,
         'date': 0,
@@ -32,6 +32,49 @@ class PayPalParser(CsvStatementParser):
         'amount': 5,
         'id': 3
     }
+    valid_header = [
+        u"Date",
+        u"Time",
+        u"Time Zone",
+        u"Name",
+        u"Type",
+        u"Status",
+        u"Currency",
+        u"Gross",
+        u"Fee",
+        u"Net",
+        u"From Email Address",
+        u"To Email Address",
+        u"Transaction ID",
+        u"Delivery address",
+        u"Address Status",
+        u"Item Title",
+        u"Item ID",
+        u"Shipping and Handling Amount",
+        u"Insurance Amount",
+        u"Sales Tax",
+        u"Option 1 Name",
+        u"Option 1 Value",
+        u"Option 2 Name",
+        u"Option 2 Value",
+        u"Reference Txn ID",
+        u"Invoice Number",
+        u"Custom Number",
+        u"Quantity",
+        u"Receipt ID",
+        u"Balance",
+        u"Address Line 1",
+        u"Address Line 2/District/Neighborhood",
+        u"Town/City",
+        u"State/Province/Region/County/Territory/Prefecture/Republic",
+        u"Zip/Postal Code",
+        u"Country",
+        u"Contact Phone Number",
+        u"Object",
+        u"Remark",
+        u"Country Code",
+        u"Impact On Balance",
+    ]
 
     unique_id_set = set()
     filetype = None
@@ -47,10 +90,10 @@ class PayPalParser(CsvStatementParser):
         """
         self._setFileType()
         stmt = super(PayPalParser, self).parse()
-        total_amount = sum(sl.amount for sl in stmt.lines)
-        stmt.start_balance = D(stmt.end_balance) - total_amount
-        stmt.start_date= min(sl.date for sl in stmt.lines)
-        statement.recalculate_balance(stmt)
+        # total_amount = sum(sl.amount for sl in stmt.lines)
+        # stmt.start_balance = D(stmt.end_balance) - total_amount
+        # stmt.start_date= min(sl.date for sl in stmt.lines)
+        # statement.recalculate_balance(stmt)
         return stmt
 
     def split_records(self):
@@ -92,6 +135,53 @@ class PayPalParser(CsvStatementParser):
 
 
     def parse_record_csv(self, line):
+        if self.date_format is None:
+            self.date_format = "%d/%m/%Y" if len(line[0].split("/")[2]) == 4 else "%Y/%m/%d"
+
+        id_idx = self.valid_header.index("Transaction ID")
+        date_idx = self.valid_header.index("Date")
+        name_idx = self.valid_header.index("Name")
+        from_idx = self.valid_header.index("Name")
+        to_idx = self.valid_header.index("Name")
+        amount_idx = self.valid_header.index("Gross")
+        currency_idx = self.valid_header.index("Currency")
+        trn_type_idx = self.valid_header.index("Impact On Balance")
+
+        if not len(line[name_idx]) and not len(line[from_idx]) and not len(line[to_idx]):
+            #Temporary  trick to skip conversion transactions
+            return None
+
+        smt_line = StatementLine()
+        smt_line.id = line[id_idx]
+        smt_line.date = datetime.strptime(line[date_idx], self.date_format)
+        smt_line.currency = line[currency_idx]
+        smt_line.amount = float(line[amount_idx].replace(',','.'))
+        if line[trn_type_idx][0:1].upper() == "C":
+            smt_line.trntype = "CREDIT"
+        elif line[trn_type_idx][0:1].upper() == "D":
+            smt_line.trntype = "DEBIT"
+
+
+        #Build memo line
+        smt_line.memo = ""
+        for column_name in [
+            "Name",
+            "Invoice Number",
+            "Currency",
+            "From Email Address",
+            "To Email Address"
+        ]:
+            memo_idx = self.valid_header.index(column_name)
+            if len(line[memo_idx]):
+                if len(smt_line.memo):
+                    smt_line.memo = smt_line.memo + " // "
+                smt_line.memo = smt_line.memo + line[memo_idx]
+
+        return smt_line
+
+
+
+
         #Valuable lines has 9 elements
         if len(line) <=9:
             if line[0] == "Opening Balance":
